872ac8380deea1b77deb3187b3a233c1e53d318b
nicolas <damien.nicolas@list.lu>
2023-11-28 18:45:30 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
-
+import * as bootstrap from 'bootstrap'
 import './assets/main.css'
+import './scss/styles.scss'
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import {Web3} from "web3";
 import {ethers} from "ethers";
 import {callTestContract} from "@/contracts";
+
 export default {
   name: 'HomeView',
   data() {
@@ -29,15 +30,6 @@ export default {
             });
       }
     },
-    // async callTestContract() {
-    //   const provider = new ethers.BrowserProvider(window.ethereum)
-    //   const signer = await provider.getSigner();
-    //   let abi = JSON.parse(`[ { "inputs": [ { "internalType": "string", "name": "newText", "type": "string" } ], "name": "changeText", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "greet", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" } ]`);
-    //   const contract = new ethers.Contract("0xCc4f72a03708CD9d2872e22c9b3E3828e669a9d3", abi, signer);
-    //   const result = await contract.greet();
-    //   console.log(result);
-    //   this.contractResult = result
-    // },
     async callChangeTextContract() {
       const provider = new ethers.BrowserProvider(window.ethereum)
       const signer = await provider.getSigner();
@@ -54,11 +46,8 @@ export default {
 <template>
   <main>
     <div>
-      <!-- connect-wallet button is visible if the wallet is not connected -->
-<!--      <button v-if="!connected">Connect wallet</button>-->
-      <!-- call-contract button is visible if the wallet is connected -->
       <p></p>
-      <button v-if="connected" @click.prevent="callTestContract">Call contract</button>
+      <button v-if="connected" @click.prevent="callTestContract" class="btn btn-primary">Call contract</button>
       {{ contractResult }}
       <p></p>
       <button v-if="connected" @click.prevent="callChangeTextContract">Call change Text</button>
